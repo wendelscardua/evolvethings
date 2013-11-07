5eb02075de493a543f74f6b51242d9fb7c6bfd4d
Wendel Scardua <wendelscardua@gmail.com>
2013-11-07 01:24:12 -0200

    início de código de GA

--- src/main/java/net/scardua/EvolveThings.java
+++ src/main/java/net/scardua/EvolveThings.java
@@ -1,5 +1,7 @@
 package net.scardua;
 
+import net.scardua.ga.GeneticAlgorithm;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,21 @@ public class EvolveThings
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        GeneticAlgorithm ga = new GeneticAlgorithm(200);
+        for(int steps = 0; steps <= 5000; steps++) {
+            if (steps > 0) {
+                ga.stepOver();
+            }
+            int countGood = 0;
+            for(GeneticAlgorithm.Chromosome chromosome : ga.getChromosomes()) {
+                double fit = 0;
+                for(GeneticAlgorithm.Value value : chromosome.getGenome()) {
+                    fit += value.getBinaryValue();
+                }
+                chromosome.setFitness(fit);
+                if (fit > 2.5) countGood++;
+            }
+            System.err.println(countGood);
+        }
     }
 }
