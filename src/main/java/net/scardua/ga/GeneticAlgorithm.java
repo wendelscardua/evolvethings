af8b1a4d55cb94d91d56895c1124d7428a04817c
Wendel Scardua <wendelscardua@gmail.com>
2013-11-07 00:11:13 -0200

    adiciona src e pom.xml

---
+++ src/main/java/net/scardua/ga/GeneticAlgorithm.java
@@ -0,0 +1,11 @@
+package net.scardua.ga;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wendel
+ * Date: 11/7/13
+ * Time: 00:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class GeneticAlgorithm {
+}
---
+++ src/test/java/net/scardua/EvolveThingsTest.java
@@ -0,0 +1,38 @@
+package net.scardua;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple EvolveThings.
+ */
+public class EvolveThingsTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public EvolveThingsTest(String testName)
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( EvolveThingsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
